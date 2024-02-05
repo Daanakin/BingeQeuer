cc4bea5735b7f369f3b578d851e56425fccbf1e8
Daan van Bragt <dws.vanbragt@student.avans.nl>
2024-02-05 11:27:18 +0100

    Initial Commit

---
+++ BingeQeuer/BingeQeuerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BingeQeuerApp.swift
+//  BingeQeuer
+//
+//  Created by Daan van Bragt on 05/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct BingeQeuerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
